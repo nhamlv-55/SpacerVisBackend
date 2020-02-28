7a39d8e82548855e1064e283ef8111d50b963c40
Nham Van Le <nv3le@precious3.eng.uwaterloo.ca>
2020-02-28 18:41:02 -0500

    first commit

---
+++ pobvis/app/main.py
@@ -0,0 +1,148 @@
+"""Style guide: use underscore for all variable and function names"""
+
+import sys
+if sys.version_info.major < 3:
+    raise Exception("User error: This application only supports Python 3, so please use python3 instead of python!")
+import json
+from flask import Flask, request
+from flask_cors import CORS
+import tempfile
+import argparse
+from chctools import horndb as H
+import io
+import os
+from settings import DATABASE, MEDIA, options_for_visualization
+from subprocess import PIPE, STDOUT, Popen, run
+from chctools import horndb as H
+from utils.utils import *
+import utils.trace_parsing as ms
+app = Flask(__name__)
+app.config.from_object(__name__)
+CORS(app)
+
+parser = argparse.ArgumentParser(description='Run Spacer Server')
+parser.add_argument("-z3", "--z3", required=True, action="store", dest="z3_path", help="path to z3 python")
+args = parser.parse_args()
+
+def update_status():
+    exps_list = []
+    for exp in query_db('select * from exp where done is 0'):
+        r = {}
+        for k in exp.keys():
+            r[k] = exp[k]
+        exps_list.append(exp["name"])
+
+    for exp in exps_list:
+        print("EXP:", exp)
+        is_running = check_if_process_running(exp)
+        if not is_running:
+            get_db().execute('UPDATE exp SET done = 1 WHERE name = ?', (exp,));
+
+    #commit
+    get_db().commit()
+
+def pooling():
+    update_status()
+    return fetch_exps()
+
+
+def start_spacer():
+    request_params = request.get_json()
+    file_content = request_params.get('file', '')
+    exp_name = request_params.get('name', '')
+    new_exp_name = get_new_exp_name(exp_name)
+    print(new_exp_name)
+    insert_db('INSERT INTO exp(name, done, result, aux, time) VALUES (?,?,?,?,?)',(new_exp_name, 0, "UNK", "NA", 0))
+
+
+    spacer_user_options = request_params.get("spacerUserOptions", "")
+
+    exp_folder = os.path.join(MEDIA, new_exp_name)
+    os.mkdir(exp_folder)
+
+    input_file = open(os.path.join(exp_folder, "input_file.smt2"), "wb")
+    input_file.write(str.encode(file_content))
+    input_file.flush() # commit file buffer to disk so that Spacer can access it
+
+    stderr_file = open(os.path.join(exp_folder, "stderr"), "w")
+    stdout_file = open(os.path.join(exp_folder, "stdout"), "w")
+
+    run_args = [args.z3_path]
+    run_args.extend(spacer_user_options.split())
+    run_args.extend(options_for_visualization)
+    run_args.append(os.path.abspath(os.path.join(exp_folder, 'input_file.smt2')))
+    print(run_args)
+
+    with open(os.path.join(exp_folder, "run_cmd"), "w") as f:
+        run_cmd = " ".join(run_args)
+        f.write(run_cmd)
+
+    Popen(run_args, stdin=PIPE, stdout=stdout_file, stderr=stderr_file, cwd = exp_folder)
+
+    return json.dumps({'status': "success", 'spacer_state': "running", 'nodes_list': {}, 'exp_name': new_exp_name})
+
+def poke():
+    #TODO: finish parsing using all the files in the exp_folder (input_file, etc.)
+    request_params = request.get_json()
+    exp_path = request_params.get('exp_path', '')
+    exp_folder = os.path.join(MEDIA, exp_path)
+    nodes_list = []
+    run_cmd = ""
+    with open(os.path.join(exp_folder, "stdout"), "r") as f:
+        stdout = f.readlines()
+    with open(os.path.join(exp_folder, "stderr"), "r") as f:
+        stderr = f.readlines()
+    with open(os.path.join(exp_folder, ".z3-trace"), "r") as f:
+        z3_trace = f.readlines()
+    with open(os.path.join(exp_folder, "spacer.log"), "r") as f:
+        spacer_log = f.readlines()
+
+    with open(os.path.join(exp_folder, "run_cmd"), "r") as f:
+        run_cmd = f.readlines()[0].strip()
+
+    spacer_state = get_spacer_state(stderr, stdout)
+    #load the file into db for parsing 
+    db = H.load_horn_db_from_file(os.path.join(exp_folder, "input_file.smt2"))
+    rels = []
+    for rel_name in db._rels:
+        rel = db.get_rel(rel_name)
+        rels.append(rel)
+
+    #TODO: only read spacer.log when there are no errors
+    if spacer_state == 'running' :
+        nodes_list = ms.parse(spacer_log)
+        #parse expr to json
+        for idx in nodes_list:
+            node = nodes_list[idx]
+            if node["exprID"]>2:
+                expr = node["expr"]
+                expr_stream = io.StringIO(expr)
+                try:
+                    ast = rels[0].pysmt_parse_lemma(expr_stream)
+                    ast_json = order_node(to_json(ast))
+                    node["ast_json"] = ast_json
+
+                except Exception as e:
+                    print("Exception when ordering the node:", e)
+                    print("Broken Node", node)
+                    print("Broken Node exprID:", node["exprID"])
+                    node["ast_json"] = {"type": "ERROR", "content": "trace is incomplete"}
+
+
+    return json.dumps({'status': "success",
+                       'spacer_state': spacer_state,
+                       'nodes_list': nodes_list,
+                       'run_cmd': run_cmd})
+
+
+@app.route('/spacer/fetch_exps', methods=['POST'])
+def handle_fetch_exps():
+    return pooling()
+@app.route('/spacer/start_iterative', methods=['POST'])
+def handle_start_spacer_iterative():
+    return start_spacer()
+@app.route('/spacer/poke', methods=['POST'])
+def handle_poke():
+    return poke()
+if __name__ == '__main__':
+    app.run()
---
+++ pobvis/app/utils/utils.py
@@ -0,0 +1,96 @@
+from flask import g
+import pysmt.operators as pyopt
+import sqlite3
+from settings import DATABASE, MEDIA, options_for_visualization
+import json
+from datetime import datetime
+import psutil
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    db.row_factory = sqlite3.Row
+    return db
+
+def query_db(query, args=(), one=False):
+    cur = get_db().execute(query, args)
+    rv = cur.fetchall()
+    cur.close()
+    return (rv[0] if rv else None) if one else rv
+def insert_db(query, args=(), one=False):
+    cur = get_db().execute(query, args)
+    get_db().commit()
+    cur.close()
+
+def fetch_exps():
+    exps_list = []
+    for exp in query_db('select * from exp'):
+        r = {}
+        for k in exp.keys():
+            r[k] = exp[k]
+        exps_list.append(r)
+    return json.dumps({'status': "success", 'exps_list':exps_list})
+
+def get_new_exp_name(exp_name):
+    now = datetime.now()
+    current_time = now.strftime("%d%m%y_%H_%M_%S")
+    return exp_name+"_"+current_time
+
+def calculate_val(node):
+    if node.is_real_constant():
+        frac = node._content.payload
+        val = frac.numerator / frac.denominator
+        return round(val, 4)
+    return str(node._content.payload[0])
+
+def to_json(node, debug = False):
+    if debug: print(node, node.get_type(), node.args())
+    if node.is_real_constant():
+        type_str = "0_REAL_CONSTANT" #hack to put real constant at the end
+    else:
+
+        type_str = pyopt.op_to_str(node.node_type())
+        if debug:
+            print("NODE TYPE:", node.node_type())
+            print("TYPE_STR:", type_str)
+    if len(node.args())==0:
+        obj = {"type": type_str, "content":calculate_val(node)}
+        return obj
+    else:
+        args = []
+        for a in node.args():
+            args.append(to_json(a, debug))
+        obj = {"type": type_str, "content": args}
+        return obj
+
+def order_node(node):
+    args = node["content"]
+    if isinstance(args, list):
+        for idx in range(len(args)):
+            args[idx] = order_node(args[idx])
+        #only order commutative operators
+        if node["type"]=="PLUS" or node["type"]=="TIMES" or node["type"]=="AND":
+            args = sorted(args, key=lambda k: (k["type"], str(k["content"])), reverse = True) 
+        node["content"] = args
+    return node
+
+def check_if_process_running(processName):
+    '''
+    Check if there is any running process that contains the given name processName.
+    '''
+    #Iterate over the all the running process
+    for proc in psutil.process_iter():
+        try:
+            # Check if process name contains the given name string.
+            cmdline = " ".join(proc.cmdline())
+            if 'z3' in cmdline:
+                print(cmdline)
+            if processName in cmdline:
+                return True
+        except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+            pass
+    return False;
+
+def get_spacer_state(stderr, stdout):
+    #TODO: implement me
+    return "running"
