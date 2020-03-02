5a22097bbf2846dd7fa3fcc51f32fcd09d2e5e97
Nham Van Le <nv3le@precious3.eng.uwaterloo.ca>
2020-03-01 22:10:39 -0500

    use datapath from env

--- pobvis/app/settings.py
+++ pobvis/app/settings.py
@@ -1,3 +1,10 @@
-DATABASE = './exp_db'
-MEDIA = './media'
+import os
+if os.environ.get('BACKEND_DATA_PATH') is not None:
+    BACKEND_DATA_PATH = os.environ['BACKEND_DATA_PATH']
+    DATABASE = os.path.join(BACKEND_DATA_PATH, 'exp_db')
+    MEDIA = os.path.join(BACKEND_DATA_PATH, 'media')
+else:
+    BACKEND_DATA_PATH = None
+    DATABASE = './exp_db'
+    MEDIA = './media'
 options_for_visualization = ["fp.spacer.trace_file=spacer.log", "fp.print_statistics=true", "-v:1"]
